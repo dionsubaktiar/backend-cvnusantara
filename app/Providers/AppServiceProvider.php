c6260fc0882aa249270e82ef3ff13ef8ed6804a3
dionsubaktiar <dionsubaktiar92@gmail.com>
2026-09-07 09:36:59 +0700

    fixing ci-cd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if ($this->app->environment('local')) {
+            if (class_exists(\Laravel\Pail\PailServiceProvider::class)) {
+                $this->app->register(\Laravel\Pail\PailServiceProvider::class);
+            }
+            if (class_exists(\Laravel\Sail\SailServiceProvider::class)) {
+                $this->app->register(\Laravel\Sail\SailServiceProvider::class);
+            }
+        }
     }
 
     /**
